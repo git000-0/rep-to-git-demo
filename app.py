9f2e63ba546dd6fc2244b2cd00a4c2c5905cba1b
git000-0 <158285608+git000-0@users.noreply.github.com>
2024-02-13 16:24:16 +0000

    web python
    web test

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+DESC = [
+  {
+    'id: ' : 1,
+    'price': '$180/g',
+    'made': '新疆',
+    'name': '苹果'
+  },
+  {
+    'id: ' : 2,
+    'price': '',
+    'made': '',
+    'name': '香蕉'
+  },
+  {
+    'id: ' : 3,
+    'price': '',
+    'made': '',
+    'name': '橘子'
+  }  
+]
+
 @app.route('/')
 def hello_world():
-      return render_template('home.html')
+      return render_template('home.html',
+                            info = DESC)
+                            
   
 if __name__ == '__main__': 
   app.run(host="0.0.0.0", debug = True)
